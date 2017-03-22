9450017bd9de515e9c026b3c4b1ee324567823f8
rjackson88 <richardwjacksonjr@yahoo.com>
2017-03-22 15:28:44 -0400

    Adding modules

---
+++ zcw-parent/src/main/java/org/eightyeight/ZcwParentApplication.java
@@ -0,0 +1,12 @@
+package org.eightyeight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZcwParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZcwParentApplication.class, args);
+	}
+}
---
+++ zcw-parent/zcw-model/src/main/java/org/eightyeight/ZcwModelApplication.java
@@ -0,0 +1,12 @@
+package org.eightyeight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZcwModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZcwModelApplication.class, args);
+	}
+}
